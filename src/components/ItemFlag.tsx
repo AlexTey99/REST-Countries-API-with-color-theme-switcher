b20fe8ac8543840e0d33ebe9935088ff9de3b598
AlexTey99 <teyalex99@gmail.com>
2023-08-11 18:23:59 +0200

    Terminacion de estilo en las banderas

--- src/components/ItemFlag.tsx
+++ src/components/ItemFlag.tsx
@@ -4,7 +4,12 @@ const ItemFlag = ({ flag}: IProps) => {
     return(
         <div className="item-flag-container">
             <img className="image-flag" src={flag.flags.png} alt="" />
-            <div className="flag-name">{flag.name.common}</div>
+            <div className="flag-dates-container">
+                <p className="name">{flag.name.common}</p>
+                <span>Population: <p className="date-text">{flag.population}</p></span>
+                <span>Region: <p className="date-text">{flag.region}</p></span>
+                <span>Capital: <p className="date-text">{flag.capital}</p></span>
+                </div>
         </div>
     );
 };
