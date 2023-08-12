8381675c15e3ff1c1fc396cf9a646f9be930ff6d
AlexTey99 <teyalex99@gmail.com>
2023-08-12 22:31:58 +0200

    implementacion del buscar pais por nombre

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,13 +1,16 @@
 
-const Input = ({className, placeholder}: IProps) => {
+const Input = ({className, placeholder, id, handleInputChange, value}: IProps) => {
     return(
-        <input className={className} type="text" placeholder={placeholder}/>
+        <input id={id}  className={className} onChange={handleInputChange}  value={value} type="text" placeholder={placeholder}/>
     );
 };
 
 type IProps = {
     className?: string;
     placeholder?: string;
+    id?: string;
+    value?: string;
+    handleInputChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 export{Input};
--- src/containers/ContainerCenter.tsx
+++ src/containers/ContainerCenter.tsx
@@ -2,32 +2,52 @@ import { Input } from "../components/Input";
 import {ItemFlag} from "../components/ItemFlag";
 import {Select} from "../components/Select";
 import { useEffect, useState } from 'react';
+import React, { KeyboardEvent, ChangeEvent } from 'react';
 
 
 const ContainerCenter = () => {
-
+    //Declaracion de variable
     const [flags, setFlags] = useState([]);
+    const [inputValue, setInputValue] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
 
     useEffect(() => {
       fetch(`https://restcountries.com/v3.1/all`)
         .then(response => response.json())
         .then(data => {
-          setFlags(data);
+          setFlags(data); // Almacena el API en un array
+          setSearchResults(data);
         })
         .catch(error => {
           console.log(error);
         });
     }, []);
 
+    useEffect(() => {
+        /// Filtrar los productos que coinciden con el término de búsqueda en el título
+          const filtered = flags.filter(product =>
+          product.name.common.toLowerCase().includes(inputValue.trim().toLowerCase())
+          );
+
+          setSearchResults(filtered);
+
+    }, [inputValue]);
+
 
     return(
         <div className="containerCenter">
             <div className="container-input">
-                <Input className="input" placeholder="Search for a country" />
+                <Input id="input"
+                value={inputValue}
+                handleInputChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setInputValue(e.target.value)
+                }  
+                className="input" placeholder="Search for a country" 
+                />
                 <Select className="selector"/>
             </div>
             <div id="containerFlags" className="container-flags">
-                {flags.map((elemetFlag) => (
+                {searchResults.map((elemetFlag) => (
                   <ItemFlag flag={elemetFlag}/>
                 ))}
             </div>
