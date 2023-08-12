cfcd7a838e58776c2320e1104b3c1cd766cf73d0
AlexTey99 <teyalex99@gmail.com>
2023-08-12 22:45:38 +0200

    ajuste de codigo

--- src/containers/ContainerCenter.tsx
+++ src/containers/ContainerCenter.tsx
@@ -2,21 +2,18 @@ import { Input } from "../components/Input";
 import {ItemFlag} from "../components/ItemFlag";
 import {Select} from "../components/Select";
 import { useEffect, useState } from 'react';
-import React, { KeyboardEvent, ChangeEvent } from 'react';
 
 
 const ContainerCenter = () => {
     //Declaracion de variable
     const [flags, setFlags] = useState([]);
     const [inputValue, setInputValue] = useState('');
-    const [searchResults, setSearchResults] = useState([]);
 
     useEffect(() => {
       fetch(`https://restcountries.com/v3.1/all`)
         .then(response => response.json())
         .then(data => {
-          setFlags(data); // Almacena el API en un array
-          setSearchResults(data);
+          setFlags(data); // Almacena los resultados de la API en un array
         })
         .catch(error => {
           console.log(error);
@@ -24,12 +21,12 @@ const ContainerCenter = () => {
     }, []);
 
     useEffect(() => {
-        /// Filtrar los productos que coinciden con el término de búsqueda en el título
-          const filtered = flags.filter(product =>
-          product.name.common.toLowerCase().includes(inputValue.trim().toLowerCase())
+        /// Filtrar los nombres de paises
+          const filtered = flags.filter(flag =>
+          flag.name.common.toLowerCase().includes(inputValue.trim().toLowerCase())
           );
 
-          setSearchResults(filtered);
+          setFlags(filtered); // Actualizo el array Flag en base al filtrado
 
     }, [inputValue]);
 
@@ -39,15 +36,14 @@ const ContainerCenter = () => {
             <div className="container-input">
                 <Input id="input"
                 value={inputValue}
-                handleInputChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                setInputValue(e.target.value)
-                }  
-                className="input" placeholder="Search for a country" 
+                handleInputChange={(e: React.ChangeEvent<HTMLInputElement>) => setInputValue(e.target.value)}  
+                className="input" 
+                placeholder="Search for a country" 
                 />
                 <Select className="selector"/>
             </div>
             <div id="containerFlags" className="container-flags">
-                {searchResults.map((elemetFlag) => (
+                {flags.map((elemetFlag) => (
                   <ItemFlag flag={elemetFlag}/>
                 ))}
             </div>
